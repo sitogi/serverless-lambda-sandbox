cc7fc513b89632c6f773080756e4e50736898a67
sitogi <takano162@gmail.com>
2024-08-08 09:44:12 +0900

    First commit (hello function works)

---
+++ src/hello.ts
@@ -0,0 +1,18 @@
+export const handle = async () => {
+  try {
+    console.log('Hello World!')
+  } catch (error) {
+    const msg = `エラー: ${error instanceof Error ? error.message : 'unknown error'}`;
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: msg,
+      }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({message: 'fire!!'}),
+  };
+};
