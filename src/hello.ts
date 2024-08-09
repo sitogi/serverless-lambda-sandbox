fc0f987982656c733b52e77ee021bce007777c87
sitogi <takano162@gmail.com>
2024-08-09 23:26:36 +0900

    chromium 足してみたけど、importModuleError で動かず

--- src/hello.ts
+++ src/hello.ts
@@ -1,6 +1,15 @@
+import puppeteer, { Browser } from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
+
 export const handle = async () => {
+  let browser: Browser | undefined;
   try {
-    console.log('Hello World!')
+    browser = await createBrowser();
+    const page = await browser.newPage();
+    await page.goto("https://google.com");
+    const pageTitle = await page.title();
+
+    console.log('ページタイトル: ', pageTitle)
   } catch (error) {
     const msg = `エラー: ${error instanceof Error ? error.message : 'unknown error'}`;
     return {
@@ -9,10 +18,28 @@ export const handle = async () => {
         message: msg,
       }),
     };
+  } finally {
+    if (browser != undefined) {
+      await browser.close();
+      console.log('ブラウザをクローズしました。')
+    }
   }
 
   return {
     statusCode: 200,
     body: JSON.stringify({message: 'fire!!'}),
   };
+};
+
+export const createBrowser = async (): Promise<Browser> => {
+  console.log('ブラウザを作成します。');
+  const options = {
+    args: [...chromium.args, '--lang=ja-JP'],
+    defaultViewport: chromium.defaultViewport,
+    headless: true,
+    executablePath: await chromium.executablePath(),
+  };
+  const browser: Browser = await puppeteer.launch(options);
+  console.log('ブラウザを作成しました。');
+  return browser;
 };
